30703dc6313117b94b97980d354e5c9d8cf347b7
Daniel Alpert <dalpert89@gmail.com>
2021-09-18 16:49:25 -0700

    added str functions for GameInfo and GameResult classes

--- game_classes.py
+++ game_classes.py
@@ -5,6 +5,10 @@ class GameInfo:
         self.home_spread = home_spread
         self.away_spread = away_spread
         self.over_under = over_under
+    def __str__(self):
+        return(self.away_team + ' (' + str(self.away_spread) + ') @ '
+               +self.home_team + '\n' +
+              'O/U: ' + str(self.over_under))
 
 class GameResults:
     def __init__(self, home_team, away_team, home_score, away_score):
@@ -14,3 +18,7 @@ class GameResults:
         self.away_score = away_score
         self.total = home_score + away_score
         self.winner = home_team if home_score > away_score else away_team if away_score > home_score else 'tie' 
+    def __str__(self):
+        return(self.home_team +' (home): '+ str(self.home_score) + '\n' +
+              self.away_team +' (away): '+ str(self.away_score) + '\n' +
+              'Total: '+ str(self.total)) 
